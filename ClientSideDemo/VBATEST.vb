159f4b14ef7a50a22ecb74b1b387533f3a75cd2e
Matteo Mazzone <73369339+mattmazzone@users.noreply.github.com>
2022-03-03 07:06:07 -0500

    Delete VBATEST.vb

--- ClientSideDemo/VBATEST.vb
+++
@@ -1,8 +0,0 @@
-Dim OutApp As Outlook.Application
-Dim objMsg As MailItem
-Set OutApp = CreateObject("Outlook.Application")
-Set objMsg = OutApp.CreateItem(olMailItem)
-With objMsg
-  .Body = messageBody
-  .Display
-End With
