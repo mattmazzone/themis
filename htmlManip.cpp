f0dfba718e4ed17f229b2b201a7e1731710ace45
Matteo Mazzone <73369339+mattmazzone@users.noreply.github.com>
2022-03-31 14:36:46 -0400

    Add files via upload

--- htmlManip.cpp
+++ htmlManip.cpp
@@ -1466,6 +1466,22 @@ void HtmlGenerator::addSubmitbuttonHtml(bool release) {
 	}
 	
 	</script>
+
+
+	<script>
+	window.onbeforeunload = function (e) {
+	  e = e || window.event;
+
+	 // For IE and Firefox prior to version 4
+	  if (e) {
+	      e.returnValue = 'Sure?';
+	  }
+
+	   // For Safari
+	   return 'Sure?';
+	};
+	</script>
+
     
 <script>
     function generatePDF(){
